9dd1711967db6831fd1121d2505f735385ebfa86
Jeremy Gibson <jgibson@caktusgroup.com>
2021-02-24 18:29:33 -0500

    Fixes webpack bug

--- {{cookiecutter.project_slug}}/webpack.local.config.js
+++ {{cookiecutter.project_slug}}/webpack.local.config.js
@@ -2,22 +2,24 @@ const path = require('path');
 const webpack = require('webpack');
 const BundleTracker = require('webpack-bundle-tracker');
 const baseConfig = require('./webpack.base.config');
-const merge = require('webpack-merge');
+const { merge } = require('webpack-merge');
 
-module.exports = merge.smart(baseConfig, {
+module.exports = merge(baseConfig, {
   mode: 'development',
   devtool: 'inline-source-map',
   output: {
-    path: path.resolve('./{{ cookiecutter.project_slug }}/static/bundles/'),
+    path: path.resolve('./{{ cookiecutter.project_slug}}/static/bundles/'),
     filename: '[name].js',
   },
   plugins: [
     new webpack.EvalSourceMapDevToolPlugin({
       exclude: /node_modules/
     }),
-    new webpack.NamedModulesPlugin(),
     new BundleTracker({
       filename: './webpack-stats.json',
     }),
-  ]
+  ],
+  optimization: {
+    moduleIds: "named",
+  }
 });
