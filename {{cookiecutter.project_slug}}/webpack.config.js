273b52371301fdd6e4a00ef0ab484d2a712b2e96
Jeremy Gibson <jgibson@caktusgroup.com>
2021-12-10 17:20:53 -0500

    Fix js webpack entry resolution

--- {{cookiecutter.project_slug}}/webpack.config.js
+++ {{cookiecutter.project_slug}}/webpack.config.js
@@ -4,7 +4,7 @@ const path = require('path');
 module.exports = {
     bail: true,
     context: __dirname,
-    entry: ['./{{cookiecutter.project_slug}}/assets/js/webpack_entry.js'],
+    entry: ['./{{cookiecutter.project_slug}}/assets/webpack_entry.js'],
     resolve: {
         extensions: ['*', '.js'],
     },
